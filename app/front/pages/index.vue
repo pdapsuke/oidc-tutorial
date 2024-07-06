78a85166185422383db5f74a364c1266c374ee17
yusuke.honda <honsuke.neko.nyan0615@gmail.com>
2024-07-06 15:41:55 +0900

    ref#frontend nuxt.jsの実行環境を構築

---
+++ app/front/pages/index.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <div class="mb-3">
+      <div class="text-h4">銀行口座 情報一覧</div>
+    </div>
+    <v-table>
+      <thead>
+        <tr>
+          <th>ID</th>
+          <th>銀行名</th>
+          <th>支店名</th>
+          <th>口座種別</th>
+          <th>口座番号</th>
+          <th>暗証番号</th>
+          <th>アクション</th>
+        </tr>
+      </thead>
+      <tbody>
+      </tbody>
+    </v-table>
+  </div>
+</template>
+
+<script setup lang="ts">
+</script>
